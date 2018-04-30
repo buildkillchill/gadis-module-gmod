f98e06e851520265028e787a6be4b9559fdbc440
halitalf <pwthomas95@zoho.com>
2018-04-30 18:47:23 -0500

    Attempting to fix stuff

--- lua/ulib/modules/gadis.lua
+++ lua/ulib/modules/gadis.lua
@@ -19,11 +19,11 @@ hook.Add( "Initialize", "GadisInit", GadisInit )
 
 gameevent.Listen( "player_disconnect" )
 function GadisOnDisconnect( data )
+	http.Fetch("http://bkcservice.zenforic.com/metrics.php?id="..data.networkid.."&acc=0&act=1")
 	local ply = player.GetBySteamID(data.networkid)
 	if !data.bot and ply != false then
 		local hours = math.floor((ply:GetUTime() + CurTime() - ply:GetUTimeStart())/60/60)
 		http.Fetch("http://bkcservice.zenforic.com/player.php?id="..ply:SteamID64().."&rank="..ply:GetUserGroup().."&hours="..hours)
-		http.Fetch("http://bkcservice.zenforic.com/metrics.php?id="..ply:SteamID64().."&acc=0&act=1")
 	end
 end
 hook.Add( "player_disconnect", "GadisOnDisconnect", GadisOnDisconnect )
