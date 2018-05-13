3ef9351e501945808b2337364d1ac4da9696446b
halitalf <pwthomas95@zoho.com>
2018-05-13 10:10:54 -0500

    New port for additional security

--- lua/ulib/modules/gadis.lua
+++ lua/ulib/modules/gadis.lua
@@ -8,7 +8,7 @@ function GadisHalfHour()
 				return
 			end
 		end
-		http.Fetch("https://bkcapi.w0lfr.net/call.php?serv="..engine.ActiveGamemode())
+		http.Fetch("https://bkcapi.w0lfr.net:7860/call.php?serv="..engine.ActiveGamemode())
 	end
 end
 
@@ -20,21 +20,21 @@ hook.Add( "Initialize", "GadisInit", GadisInit )
 gameevent.Listen( "player_disconnect" )
 function GadisOnDisconnect( data )
 	if !data.bot then
-		http.Fetch("https://bkcapi.w0lfr.net/metrics.php?id="..data.networkid.."&acc=0&act=1")
+		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..data.networkid.."&acc=0&act=1")
 		local uid = util.CRC("gm_" .. data.networkid .. "_gm")
 		local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
 		local time = row and row.totaltime
 		local grp = ucl.getUserInfoFromID(uid)["group"]
 		local hours = math.floor(time/60/60)
 		local s64 = util.SteamIDTo64(data.networkid)
-		http.Fetch("https://bkcapi.w0lfr.net/player.php?id="..s64.."&rank="..grp.."&hours="..hours)
+		http.Fetch("https://bkcapi.w0lfr.net:7860/player.php?id="..s64.."&rank="..grp.."&hours="..hours)
 	end
 end
 hook.Add( "player_disconnect", "GadisOnDisconnect", GadisOnDisconnect )
 
 function GadisPlayerInitialSpawn( ply )
 	if ply:SteamID() != "BOT" then
-		http.Fetch("https://bkcapi.w0lfr.net/metrics.php?id="..ply:SteamID().."&acc=0&act=0")
+		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..ply:SteamID().."&acc=0&act=0")
 	end
 end
 hook.Add( "PlayerInitialSpawn", "GadisPlayerInitialSpawn", GadisPlayerInitialSpawn )
