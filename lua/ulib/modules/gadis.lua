150debeafc3473234001872181457117a75ab52b
halitalf <pwthomas95@zoho.com>
2018-05-13 15:55:04 -0500

    Added console logging to debug

--- lua/ulib/modules/gadis.lua
+++ lua/ulib/modules/gadis.lua
@@ -19,7 +19,9 @@ hook.Add( "Initialize", "GadisInit", GadisInit )
 
 gameevent.Listen( "player_disconnect" )
 function GadisPlayerDisconnect( data )
+	print("Player with ID " .. data.networkid .. " has disconnected.")
 	if not data.bot then
+		print("ID " .. data.networkid .. " was determined to be human, so disconnect will be logged")
 		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..data.networkid.."&acc=0&act=1")
 		local uid = util.CRC("gm_" .. data.networkid .. "_gm")
 		local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
@@ -34,7 +36,9 @@ hook.Add( "player_disconnect", "GadisPlayerDisconnect", GadisPlayerDisconnect )
 
 gameevent.Listen( "player_connect" )
 function GadisPlayerConnect( data )
+	print("Player with ID " .. data.networkid .. " has connected.")
 	if not data.bot then
+		print("ID " .. data.networkid .. " was determined to be human, so connection will be logged")
 		local s64 = util.SteamIDTo64(data.networkid)
 		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..s64.."&acc=0&act=0")
 	end
