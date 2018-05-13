7eb474b5264a2da924a34cb009c2fd343a264955
halitalf <pwthomas95@zoho.com>
2018-05-13 15:46:08 -0500

    added console command gadistime to get the stored UTime of a player

--- lua/ulx/modules/sh/gadislink.lua
+++ lua/ulx/modules/sh/gadislink.lua
@@ -17,3 +17,14 @@ link:addParam{ type = ULib.cmds.NumArg, min = 10000, max = 99999, hint = "Link C
 
 link:help("Used to confirm the linking process for the Gadis bot.")
 
+function ulx.gadistime(Admin, ply)
+	local uid = ply:UniqueID()
+	local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
+	local time = row and row.totaltime
+	time = math.floor(time / 60 / 60)
+	ULib.tsay(Admin, ply:Nick() .. "'s UTime is: " .. time, true)
+end
+local gadistime = ulx.command("Utility", "ulx gadistime", ulx.gadistime, "!gadistime")
+gadistime:defaultAccess(ULib.ACCESS_ADMIN)
+gadistime:addParam{ type = ULib.cmds.PlayerArg }
+gadistime:help("Prints the UTime in hours of the player.")
