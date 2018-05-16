88bf8dfe2220ed783fa52ebd2acabebe046fd899
zenforic <zenforic@gmail.com>
2018-05-16 01:34:56 -0500

    Beautified code, added ignore for main branch.

--- lua/ulib/modules/gadis.lua
+++ lua/ulib/modules/gadis.lua
@@ -1,48 +1,48 @@
 if CLIENT then return end
 
 function GadisHalfHour()
-	local count = #player.GetHumans()
-	if count > 0 then
-		for k,v in pairs(player.GetHumans()) do
-			if v:IsAdmin() then
-				return
-			end
-		end
-		http.Fetch("https://bkcapi.w0lfr.net:7860/call.php?serv="..engine.ActiveGamemode())
-	end
+  local count = #player.GetHumans()
+  if count > 0 then
+    for k, v in pairs(player.GetHumans()) do
+      if v:IsAdmin() then
+        return
+      end
+    end
+    http.Fetch("https://bkcapi.w0lfr.net:7860/call.php?serv=" .. engine.ActiveGamemode())
+  end
 end
 
 function GadisInit()
-	timer.Create( "GadisHalfHour", 1800, 0, GadisHalfHour )
+  timer.Create( "GadisHalfHour", 1800, 0, GadisHalfHour )
 end
 hook.Add( "Initialize", "GadisInit", GadisInit )
 
 gameevent.Listen( "player_disconnect" )
 function GadisPlayerDisconnect( data )
-	if not data.networkid:lower():find("bot") then
-		print("A HUMAN HAS DISCONNECTED! ID: " .. data.networkid)
-		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..data.networkid.."&acc=0&act=1")
-		local uid = util.CRC("gm_" .. data.networkid .. "_gm")
-		local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
-		local time = row and row.totaltime
-		local grp = ucl.getUserInfoFromID(uid)["group"]
-		local hours = math.floor(time/60/60)
-		local s64 = util.SteamIDTo64(data.networkid)
-		http.Fetch("https://bkcapi.w0lfr.net:7860/player.php?id="..s64.."&rank="..grp.."&hours="..hours)
-	else
-		print("A BOT DISCONNECTED!")
-	end
+  if not data.networkid:lower():find("bot") then
+    print("A HUMAN HAS DISCONNECTED! ID: " .. data.networkid)
+    http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id=" .. data.networkid .. "&acc=0&act=1")
+    local uid = util.CRC("gm_" .. data.networkid .. "_gm")
+    local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
+    local time = row and row.totaltime
+    local grp = ucl.getUserInfoFromID(uid)["group"]
+    local hours = math.floor(time / 60 / 60)
+    local s64 = util.SteamIDTo64(data.networkid)
+    http.Fetch("https://bkcapi.w0lfr.net:7860/player.php?id=" .. s64 .. "&rank=" .. grp .. "&hours=" .. hours)
+  else
+    print("A BOT DISCONNECTED!")
+  end
 end
 hook.Add( "player_disconnect", "GadisPlayerDisconnect", GadisPlayerDisconnect )
 
 gameevent.Listen( "player_connect" )
 function GadisPlayerConnect( data )
-	if not data.networkid:lower():find("bot") then
-		print("A HUMAN HAS CONNECTED! ID: " .. data.networkid)
-		local s64 = util.SteamIDTo64(data.networkid)
-		http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id="..s64.."&acc=0&act=0")
-	else
-		print("A BOT CONNECTED!")
-	end
+  if not data.networkid:lower():find("bot") then
+    print("A HUMAN HAS CONNECTED! ID: " .. data.networkid)
+    local s64 = util.SteamIDTo64(data.networkid)
+    http.Fetch("https://bkcapi.w0lfr.net:7860/metrics.php?id=" .. s64 .. "&acc=0&act=0")
+  else
+    print("A BOT CONNECTED!")
+  end
 end
 hook.Add( "player_connect", "GadisPlayerConnect", GadisPlayerConnect )
--- lua/ulx/modules/sh/gadislink.lua
+++ lua/ulx/modules/sh/gadislink.lua
@@ -1,13 +1,13 @@
 function ulx.link(ply, linkCode)
   local steamid = ply:SteamID64()
   http.Fetch("http://bkcservice.zenforic.com/link.php?id=" .. steamid .. "&code=" .. linkCode,
-	function( body, len, headers, code )
-		ULib.tsay( ply, "You are now linked " .. ply:Nick() .. ", thank you." )
-	end,
-	function( error )
-		ULib.tsayError( ply, "Link failed, error: " .. error, true )
-	end
- )
+    function( body, len, headers, code )
+      ULib.tsay( ply, "You are now linked " .. ply:Nick() .. ", thank you." )
+    end,
+    function( error )
+      ULib.tsayError( ply, "Link failed, error: " .. error, true )
+    end
+  )
 end
 local link = ulx.command("Utility", "ulx link", ulx.link, "!link")
 link:defaultAccess(ULib.ACCESS_ALL)
@@ -18,11 +18,11 @@ link:addParam{ type = ULib.cmds.NumArg, min = 10000, max = 99999, hint = "Link C
 link:help("Used to confirm the linking process for the Gadis bot.")
 
 function ulx.gadistime(Admin, ply)
-	local uid = ply:UniqueID()
-	local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
-	local time = row and row.totaltime
-	time = math.floor(time / 60 / 60)
-	ULib.tsay(Admin, ply:Nick() .. "'s UTime is: " .. time, true)
+  local uid = ply:UniqueID()
+  local row = sql.QueryRow("SELECT totaltime FROM utime WHERE player = " .. uid .. ";")
+  local time = row and row.totaltime
+  time = math.floor(time / 60 / 60)
+  ULib.tsay(Admin, ply:Nick() .. "'s UTime is: " .. time, true)
 end
 local gadistime = ulx.command("Utility", "ulx gadistime", ulx.gadistime, "!gadistime")
 gadistime:defaultAccess(ULib.ACCESS_ADMIN)
